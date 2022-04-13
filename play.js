a228e7d78fa2a0b170820097e89ddbe0c0fac225
Nhan <nktran4@gmail.com>
2022-04-13 15:30:05 -0400

    added control

--- play.js
+++ play.js
@@ -2,4 +2,32 @@ const net = require("net");
 const stdin = process.stdin;
 const connect = require(`./client.js`)
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function () {
+    if (key === "a") {
+      conn.write("Move: left");
+    }
+  
+    if (key === "s") {
+      conn.write("Move: down");
+    }
+  
+    if (key === "d") {
+      conn.write("Move: right");
+    }
+  
+    if (key === "w") {
+      conn.write("Move: up");
+    }
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
